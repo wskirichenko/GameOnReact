6105f7d6e76e332f3042c3fb9afc48b79adac9f8
wskirichenko <ws.kirichenko@gmail.com>
2019-11-25 12:10:14 +0300

    Added meny items

--- game-react/src/main/MainContent.js
+++ game-react/src/main/MainContent.js
@@ -4,7 +4,11 @@ import './MainContent.scss';
 const MainContent = () => {
     return (
       <div className="MainContent">
-        <h2>Главная страница</h2>
+        <h2>Страница выбора команд</h2>
+        {/* <Route exact path='/'  render={ ProfileComponent }/>
+        <Route path='/profile' render={ ProfileComponent }/>
+        <Route path='/dialog'  render={ DialogComponent }/>
+        <Route path='/news'    component={ News }/> */}
       </div>
     );
 }
--- game-react/src/meny/Meny.js
+++ game-react/src/meny/Meny.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Meny.scss';
 import logo from '../../src/logo.svg';
-
+import { NavLink } from "react-router-dom";
 
 const Meny = () => {
     return (
       <div className="Meny">
         <img src={logo} className="Meny-logo" alt="logo" />
         <h2>Meny</h2>
+        <nav className="Meny__items">
+          <NavLink className="nav" exact to="/">Страница выбора команд</NavLink>
+          <NavLink className="nav" to="/tems">Страница выбора тем</NavLink>
+          <NavLink className="nav" to="/main">Главная страница</NavLink>
+          <NavLink className="nav" to="/questens">Страница выброного вопроса</NavLink>
+          <NavLink className="nav" to="/results">Страница Результата</NavLink>
+        </nav>
       </div>
     );
 }
